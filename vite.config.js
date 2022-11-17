f34c80708f62579bf1e53d8a7dee77531da24f52
Umair Ali <87652686+ali-umair@users.noreply.github.com>
2022-11-17 14:05:27 -0500

    some changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ali-umair.github.io/solinfo",
+  base: '/solinfo/',
   plugins: [react()]
 })
