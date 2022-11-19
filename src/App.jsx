bb81596b17bc623af6886df5028ba1d90d14bae1
Umair Ali <87652686+ali-umair@users.noreply.github.com>
2022-11-19 13:47:33 -0500

    Card UI ready

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,23 @@ import Card from './components/Card'
 
 function App() {
 
+  const planets = {
+    "id": "terre",
+    "name": "La Terre",
+    "englishName": "Earth",
+    "isPlanet": true,
+    "moons": [
+    {}
+    ],
+    "gravity": 9.8,
+    "escape": 11190,
+    "bodyType": "Planet"
+}
+
   return (
-    <>
-    <Card />
-    </>
+    <div className='flex flex-col items-center'>
+      <Card data={planets} />
+    </div>
   )
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,50 +1,51 @@
 import React from 'react'
 
-export default function Card() {
-  return (
-    <div class="flex justify-center">
-        <div class="max-w-7xl grid grid-cols-1 px-4 py-2 my-20 gap-y-10 gap-x-12  md:grid-cols-2 xl:grid-cols-3">
-            <div class="w-[300px] h-auto border-[3px] hover:border-green-400 rounded-xl shadow-xl duration-500 ease-in-out hover:shadow-2xl xl:ml-[5px]">
+// https://api.le-systeme-solaire.net/rest/bodies?filter[]=isPlanet,eq,true&data=id,name,englishName,isPlanet,moons,desity,gravity,escape,bodyType
+// https://api.le-systeme-solaire.net/rest/bodies/Uranus?data=moons,moon,rel
+
+export default function Card(props) {
+    return (
+        <div className="max-w-7xl px-4 py-2 my-2">
+            <div className="w-[300px] h-auto border-[3px] hover:border-green-400 rounded-xl shadow-xl duration-500 ease-in-out hover:shadow-2xl xl:ml-[5px]">
                 <div
-                    class="border-b border-slate-200 bg-white rounded-md shadow-md relative z-10 divide-y divide-solid divide-gray-300 space-y-9">
+                    className="border-b border-slate-200 bg-white rounded-md shadow-md relative z-10 divide-y divide-solid divide-gray-300 space-y-9">
                     <div>
-                        <div
-                            class="uppercase tracking-wider text-white text-center rounded-md w-[320px] -ml-[13px] mt-5 p-3 border border-green-400 bg-green-400 font-bold">
-                            basic</div>
-                        <div class="w-[150px] mt-8 mb-4 ml-[72px]">
-                            <img src="https://img.freepik.com/free-vector/beautiful-hand-draw-landscape-with-mountain-sketch-design_1035-20277.jpg?t=st=1656619535~exp=1656620135~hmac=f10f65b508250b926c1a72d1a699fc5c1b112e2934016ec2e3250c8f175395f8&w=900"
-                                alt="" srcset="" class="" />
+                        <div className="uppercase tracking-wider text-white text-center rounded-md w-[320px] -ml-[13px] mt-5 mb-5 p-3 border border-green-400 bg-green-400 font-bold">
+                            {props.data.bodyType}
                         </div>
-                        <div class="flex justify-center tracking-wider text-green-400">
-                            <div class="text-xl font-medium mt-[6px]">$</div>
-                            <div class="text-6xl font-light">5</div>
-                            <div class="text-xl font-medium mt-[32px]">/month</div>
+                        <div className="flex justify-center tracking-wider text-green-400">
+                            <div className="text-3xl font-light">{props.data.englishName}</div>
                         </div>
                     </div>
 
-                    <div class="w-[230px] ml-8">
-                        <div class="flex-col  text-center space-y-2 my-5">
-                            <div class="flex justify-center text-sm font-bold text-blue-500 space-x-1">
-                                <ion-icon name="checkmark-sharp" class="mt-[3px]"></ion-icon>
-                                <div class=""> Lorem ipsum</div>
+                    <div className="w-[230px] ml-8">
+                        <div className="flex-col text-center space-y-3 my-5">
+                            <div>
+                                <div className="text-xs font-thin tracking-widest text-slate-500">Scientific Name</div>
+                                <div className="text-xs font-bold tracking-widest text-slate-500">{props.data.name}</div>
                             </div>
-                            <div class="flex justify-center text-sm font-bold text-blue-500 space-x-1">
-                                <ion-icon name="checkmark-sharp" class="mt-[3px]"></ion-icon>
-                                <div class=""> Lorem ipsum dolor sit.</div>
+                            <div>
+                                <div className="text-xs font-thin tracking-widest text-slate-500">Moons</div>
+                                <div className="text-xs font-bold tracking-widest text-slate-500">{props.data.moons.length}</div>
                             </div>
-                            <div class="text-sm font-bold text-gray-500">Lorem ipsum </div>
+                            <div>
+                                <div className="text-xs font-thin tracking-widest text-slate-500">Gravity</div>
+                                <div className="text-xs font-bold tracking-widest text-slate-500">{props.data.gravity} <span>m/s<sup>2</sup></span></div>
+                            </div>
+                            <div>
+                                <div className="text-xs font-thin tracking-widest text-slate-500">Escape Velocity</div>
+                                <div className="text-xs font-bold tracking-widest text-slate-500">{props.data.escape} <span>m/s</span></div>
+                            </div>
                         </div>
                     </div>
                 </div>
 
                 <a href="#">
-                    <div
-                        class="uppercase text-green-400 text-center rounded-b-md w-auto pt-8 pb-[30px] text-sm  font-bold -mt-2 relative z-0 hover:cursor-pointer hover:text-green-600 hover:bg-green-200">
-                        current subscription
+                    <div className="uppercase text-green-400 text-center rounded-b-md w-auto pt-8 pb-[30px] text-xs font-bold -mt-2 relative z-0 hover:cursor-pointer hover:text-green-600 hover:bg-green-200">
+                        Learn more about {props.data.englishName}
                     </div>
                 </a>
             </div>
         </div>
-    </div>
-  )
-}
+    )
+    }
