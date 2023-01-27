0b30275ba99c0f1a08e4300d2a5142fb73d8759f
ali-umair <87652686+ali-umair@users.noreply.github.com>
2023-01-27 18:20:54 +0500

    Base grid ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,73 @@
-import Home from './components/Home'
-import Navbar from './components/navbar/Navbar'
-import {
-  HashRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import Home from './components/Home';
+import Navbar from './components/navbar/Navbar';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Redesign from './components/Redesign';
 
 export default function App() {
-
-
   const Earth = {
-    "id": "terre",
-    "name": "La Terre",
-    "englishName": "Earth",
-    "isPlanet": true,
-    "moons": [
-    {}
-    ],
-    "gravity": 9.8,
-    "escape": 11190,
-    "bodyType": "Planet"
-}
+    id: 'terre',
+    name: 'La Terre',
+    englishName: 'Earth',
+    isPlanet: true,
+    moons: [{}],
+    gravity: 9.8,
+    escape: 11190,
+    bodyType: 'Planet',
+  };
   const Mercury = {
-    "id": "mercure",
-    "name": "Mercure",
-    "englishName": "Mercury",
-    "isPlanet": true,
-    "moons": null,
-    "gravity": 3.7,
-    "escape": 4250,
-    "bodyType": "Planet"
-  }
+    id: 'mercure',
+    name: 'Mercure',
+    englishName: 'Mercury',
+    isPlanet: true,
+    moons: null,
+    gravity: 3.7,
+    escape: 4250,
+    bodyType: 'Planet',
+  };
   const Venus = {
-    "id": "venus",
-    "name": "Vénus",
-    "englishName": "Venus",
-    "isPlanet": true,
-    "moons": null,
-    "gravity": 8.87,
-    "escape": 10360,
-    "bodyType": "Planet"
-  }
+    id: 'venus',
+    name: 'Vénus',
+    englishName: 'Venus',
+    isPlanet: true,
+    moons: null,
+    gravity: 8.87,
+    escape: 10360,
+    bodyType: 'Planet',
+  };
 
   return (
-    <div className='bg-[#222327] h-screen flex flex-col justify-between'>
-      <Router>
-      <Routes>
-        <Route exact path="/" element={<Home body={Earth} select="planets" />} />
-        <Route exact path="/dwarfs" element={<Home body={Mercury} select="dwarfs" />} />
-        <Route exact path="/moons" element={<Home body={Venus} select="moons"/>} />
-        <Route exact path="/asteroids" element={<Home body={Earth} select="asteroids"/>} />
-        <Route exact path="/comets" element={<Home body={Venus} select="comets"/>} />
-      </Routes>
-      <Navbar />
-    </Router>
-    </div>
-  )
+    <Redesign />
+    // <div className="bg-[#222327] h-screen flex flex-col justify-between">
+    //   <Router>
+    //     <Routes>
+    //       <Route
+    //         exact
+    //         path="/"
+    //         element={<Home body={Earth} select="planets" />}
+    //       />
+    //       <Route
+    //         exact
+    //         path="/dwarfs"
+    //         element={<Home body={Mercury} select="dwarfs" />}
+    //       />
+    //       <Route
+    //         exact
+    //         path="/moons"
+    //         element={<Home body={Venus} select="moons" />}
+    //       />
+    //       <Route
+    //         exact
+    //         path="/asteroids"
+    //         element={<Home body={Earth} select="asteroids" />}
+    //       />
+    //       <Route
+    //         exact
+    //         path="/comets"
+    //         element={<Home body={Venus} select="comets" />}
+    //       />
+    //     </Routes>
+    //     <Navbar />
+    //   </Router>
+    // </div>
+  );
 }
---
+++ src/components/Redesign.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// https://api.le-systeme-solaire.net/rest/bodies?filter[]=isPlanet,eq,true&data=id,name,englishName,isPlanet,moons,desity,gravity,escape,bodyType
+// https://api.le-systeme-solaire.net/rest/bodies/Uranus?data=moons,moon,rel
+
+export default function Redesign() {
+  return (
+    <div className="bg-black h-screen">
+      <div className="bg-white h-full w-1/4"></div>
+      <div className=""></div>
+    </div>
+  );
+}
