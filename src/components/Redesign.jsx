dbfb95318dc12b9e08c50dc62c9f61ccf6d77669
UmairAli <17-CP-62@students.uettaxila.edu.pk>
2023-01-31 21:12:56 +0000

    UI-MVP

--- src/components/Redesign.jsx
+++ src/components/Redesign.jsx
@@ -12,7 +12,7 @@ export default function Redesign() {
 	};
 
 	return (
-		<div className="bg-black h-screen flex">
+		<div className="bg-black h-screen flex font-mono">
 			<div className="bg-white h-full w-1/4 pt-5 text-center">
 				<button
 					onClick={click}
@@ -141,8 +141,42 @@ export default function Redesign() {
 					<div className="bg-orange-700 h-6 w-6 rounded-full hover:scale-110"></div>
 				</div>
 			</div>
-			<div className="text-white h-full w-3/4 pt-5 text-center">
-				<h1>Details</h1>
+			<div className="text-white h-full w-3/4 p-5">
+				<h1 className="text-xl underline underline-offset-4 decoration-red-500">Mercury</h1>
+				<div className="container_details pt-10 flex flex-wrap justify-start">
+					<div className="detail mb-5 flex">
+						<div className="text-red-500 text-center border-r-2 w-44">Name</div>
+						<div className="text-center w-44">Mercury</div>
+					</div>
+					<div className="detail mb-5 flex">
+						<div className="text-red-500 text-center border-r-2 w-44">Moons</div>
+						<div className="text-center w-44">4 km</div>
+					</div>
+					<div className="detail mb-5 flex">
+						<div className="text-red-500 text-center border-r-2 w-44">Discovered By</div>
+						<div className="text-center w-44">45687913</div>
+					</div>
+					<div className="detail mb-5 flex">
+						<div className="text-red-500 text-center border-r-2 w-44">Name</div>
+						<div className="text-center w-44">Mercury</div>
+					</div>
+					<div className="detail mb-5 flex">
+						<div className="text-red-500 text-center border-r-2 w-44">Moons</div>
+						<div className="text-center w-44">4</div>
+					</div>
+					<div className="detail mb-5 flex">
+						<div className="text-red-500 text-center border-r-2 w-44">Discovered By</div>
+						<div className="text-center w-44">45687913</div>
+					</div>
+					<div className="detail mb-5 flex">
+						<div className="text-red-500 text-center border-r-2 w-44">Name</div>
+						<div className="text-center w-44">Mercury</div>
+					</div>
+					<div className="detail mb-5 flex">
+						<div className="text-red-500 text-center border-r-2 w-44">Moons</div>
+						<div className="text-center w-44">4</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
