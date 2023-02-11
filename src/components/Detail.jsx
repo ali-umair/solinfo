c3f719812ac4b01aa121d21ba179ae2816be0f92
UmairAli <17-CP-62@students.uettaxila.edu.pk>
2023-02-11 15:24:04 +0000

    Details page MVP ready - exluded some fields

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,48 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-export default function Detail() {
+export default function Detail(props) {
+	const generateItems = () => {
+		let dataList = [];
+		Object.entries(props.starData).forEach(([key, value]) => {
+			dataList.push(
+				<div className="detail mb-10 flex">
+					<div className="text-red-500 text-center border-r-2 w-44">{key}</div>
+					<div className="text-center w-44">{value}</div>
+				</div>
+			);
+		});
+		return dataList;
+	};
 	return (
 		<>
 			<h1 className="text-xl underline underline-offset-4 decoration-red-500">
-				Mercury
+				{props.starData.englishName}
 			</h1>
 			<div className="container_details pt-10 flex flex-wrap justify-start">
-				<div className="detail mb-5 flex">
-					<div className="text-red-500 text-center border-r-2 w-44">Name</div>
-					<div className="text-center w-44">Mercury</div>
-				</div>
-				<div className="detail mb-5 flex">
-					<div className="text-red-500 text-center border-r-2 w-44">Moons</div>
-					<div className="text-center w-44">4 km</div>
-				</div>
-				<div className="detail mb-5 flex">
-					<div className="text-red-500 text-center border-r-2 w-44">
-						Discovered By
-					</div>
-					<div className="text-center w-44">45687913</div>
-				</div>
-				<div className="detail mb-5 flex">
-					<div className="text-red-500 text-center border-r-2 w-44">Name</div>
-					<div className="text-center w-44">Mercury</div>
-				</div>
-				<div className="detail mb-5 flex">
-					<div className="text-red-500 text-center border-r-2 w-44">Moons</div>
-					<div className="text-center w-44">4</div>
-				</div>
-				<div className="detail mb-5 flex">
-					<div className="text-red-500 text-center border-r-2 w-44">
-						Discovered By
-					</div>
-					<div className="text-center w-44">45687913</div>
-				</div>
-				<div className="detail mb-5 flex">
-					<div className="text-red-500 text-center border-r-2 w-44">Name</div>
-					<div className="text-center w-44">Mercury</div>
-				</div>
-				<div className="detail mb-5 flex">
-					<div className="text-red-500 text-center border-r-2 w-44">Moons</div>
-					<div className="text-center w-44">4</div>
-				</div>
+				{generateItems()}
 			</div>
 		</>
 	);
--- src/components/Redesign.jsx
+++ src/components/Redesign.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Detail from "./Detail";
 import Sidebar from "./Sidebar";
 
@@ -6,12 +6,13 @@ import Sidebar from "./Sidebar";
 // https://api.le-systeme-solaire.net/rest/bodies/Uranus?data=moons,moon,rel
 
 export default function Redesign() {
+	const [starData, setStarData] = useState("");
 	function starSelectData(star) {
 		fetch(
-			`https://api.le-systeme-solaire.net/rest/bodies?filter[]=englishName,eq,${star}`
+			`https://api.le-systeme-solaire.net/rest/bodies?filter[]=englishName,eq,${star}&exclude=mass,vol,moons,rel,dimension,aroundPlanet,discoveredBy,discoveryDate,alternativeName,isPlanet,bodyType`
 		)
 			.then((response) => response.json())
-			.then((data) => console.log(data.bodies[0]))
+			.then((data) => setStarData(data.bodies[0]))
 			.catch((error) => {
 				console.log(error);
 			});
@@ -19,10 +20,10 @@ export default function Redesign() {
 	return (
 		<div className="bg-black h-screen flex font-mono">
 			<div className="bg-white text-slate-700 h-full w-1/4 pt-5 text-center">
-				<Sidebar starSelectData={starSelectData}/>
+				<Sidebar starSelectData={starSelectData} />
 			</div>
 			<div className="text-white h-full w-3/4 p-5">
-				<Detail />	
+				<Detail starData={starData} />
 			</div>
 		</div>
 	);
